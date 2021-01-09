2285ff5d0e3f0475ee0517eb698b63fea83e831a
adelazalewski <paliuadela@yahoo.com>
2021-01-09 16:44:16 -0500

    seted up project

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
 // build your `/api/projects` router here
+const express = require("express");
+const model = require("./model");
+const router = express.Router();
+router.get("/projects", async (req, res, next) => {
+    try{
+        
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
+})
+
+module.exports = router;
